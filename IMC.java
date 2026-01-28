90e2f9791eb7a7eb66915e87df3326613fcbfb4a
GabrielDuarte-collab <joaogabr44@gmail.com>
2026-01-28 00:51:33 +0000

    imc

---
+++ IMC.java
@@ -0,0 +1,10 @@
+public class IMC {
+    public static double calcularIMC (double altura, double peso) {
+        return peso /(altura * altura);
+    } 
+    public static void classificaIMC (double IMC) {
+        if(IMC < 18.5) {
+            return "Abaixo do peso"
+        }else if (IMC <= 18.5 )
+    }
+}
